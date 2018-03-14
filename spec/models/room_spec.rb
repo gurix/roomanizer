76b7469d55f381151f9cd778498f2f2cd15ae680
Markus Graf <info@markusgraf.ch>
2018-03-14 10:38:22 +0100

    Makeup

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Room do
-  it {is_expected.to validate_presence_of(:title)}
-  it {is_expected.to belong_to(:floor)}
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to belong_to(:floor) }
 end
