197b4197eb9967cd0c7f0f400196b5327816c4e8
Markus Graf <info@markusgraf.ch>
2018-03-16 14:23:29 +0100

    Refactor booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,11 +6,39 @@ class Booking < ApplicationRecord
 
   validates :start_at, :end_at, presence: true
   validates_datetime :end_at, after: :start_at
-  validate :availability
+  validate :availability_start
+  validate :availability_end
+  validate :availability_within
 
-  def availability
-    conflicting_bookings = self.class.where('(start_at BETWEEN ? AND ?) OR (end_at BETWEEN ? AND ?)', start_at, end_at, start_at, end_at)
-    conflicting_bookings.where(bookable: bookable)
-    errors.add(:bookable, :not_available) if conflicting_bookings.count > 0
+  # Checks whether there is a start from another booking in between
+  def availability_start
+    conflicting_bookings = other_bookings.where('start_at BETWEEN ? AND ?', start_at, end_at)
+    errors.add(:start_at, I18n.t('activerecord.errors.models.booking.attributes.start_at.not_available',
+      time: I18n.l(conflicting_bookings.first.start_at))) if conflicting_bookings.count > 0
+  end
+
+  # Checks whether there is an end from another booking in between
+  def availability_end
+    # Query for bookings ending in the requestet time period
+    conflicting_bookings = other_bookings.where('end_at BETWEEN ? AND ?', start_at, end_at)
+    errors.add(:end_at, I18n.t('activerecord.errors.models.booking.attributes.end_at.not_available',
+      time: I18n.l(conflicting_bookings.first.end_at))) if conflicting_bookings.count > 0
+  end
+
+  # Checks whether there is a start and an end from another booking in between
+  def availability_within
+    # Query for bookings ending in the requestet time period
+    conflicting_bookings = other_bookings.where('start_at <= ? AND end_at >= ?', start_at, end_at)
+    if conflicting_bookings.count > 0
+      errors.add(:start_at, I18n.t('activerecord.errors.models.booking.attributes.start_at.not_available',
+        time: I18n.l(conflicting_bookings.first.start_at)))
+      errors.add(:end_at, I18n.t('activerecord.errors.models.booking.attributes.end_at.not_available',
+        time: I18n.l(conflicting_bookings.first.end_at)))
+    end
+  end
+
+  # Filter for all bookings for a given resource not beeing itself
+  def other_bookings
+    self.class.where(bookable: bookable).where.not(id: id)
   end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -13,20 +13,45 @@ RSpec.describe Booking do
     expect(booking.errors.full_messages).to include 'End at must be after 2015-06-15 14:33:52'
   end
 
-  it 'validates availability' do
-    room = create :room, bookings:[
-      Booking.new(start_at: Time.now + 1.hour, end_at: Time.now + 3.hour)
-    ]
+  describe 'availability' do
+    before { @room = create :room, bookings:[ Booking.new(start_at: Time.now + 1.hour, end_at: Time.now + 3.hour) ]}
 
-    valid_booking = Booking.new(bookable: room, start_at: Time.now - 2.hours, end_at: Time.now - 1.hours)
-    expect(valid_booking).to be_valid
+    it 'raises an error if there is a start from another booking in between' do
+      valid_booking = Booking.new(bookable: @room, start_at: Time.now - 2.hours, end_at: Time.now - 1.hours)
+      expect(valid_booking).to be_valid
 
-    invalid_booking = Booking.new(bookable: room, start_at: Time.now + 2.hours, end_at: Time.now + 4.hours)
-    expect(invalid_booking).not_to be_valid
-    expect(invalid_booking.errors.full_messages).to include('Location not available for that time')
+      invalid_booking = Booking.new(bookable: @room, start_at: Time.now + 30.minutes, end_at: Time.now + 2.hours)
+      expect(invalid_booking).not_to be_valid
+      expect(invalid_booking.errors.full_messages).to include('Start at reserved from Mon, 15 Jun 2015 15:33:52 +0200')
+    end
 
-    another_invalid_booking = Booking.new(bookable: room, start_at: Time.now + 30.minutes, end_at: Time.now + 2.hours)
-    expect(another_invalid_booking).not_to be_valid
-    expect(another_invalid_booking.errors.full_messages).to include('Location not available for that time')
+    it 'raises an error if there is an end from another booking in between' do
+      valid_booking = Booking.new(bookable: @room, start_at: Time.now - 2.hours, end_at: Time.now - 1.hours)
+      expect(valid_booking).to be_valid
+
+      invalid_booking = Booking.new(bookable: @room, start_at: Time.now + 2.hours, end_at: Time.now + 4.hours)
+      expect(invalid_booking).not_to be_valid
+      expect(invalid_booking.errors.full_messages).to include('End at reserved until Mon, 15 Jun 2015 17:33:52 +0200')
+    end
+
+    it 'raises an error if there is another booking in between' do
+      valid_booking = Booking.new(bookable: @room, start_at: Time.now - 2.hours, end_at: Time.now - 1.hours)
+      expect(valid_booking).to be_valid
+
+      invalid_booking = Booking.new(bookable: @room, start_at: Time.now + 30.minutes, end_at: Time.now + 4.hours)
+      expect(invalid_booking).not_to be_valid
+      expect(invalid_booking.errors.full_messages).to include('Start at reserved from Mon, 15 Jun 2015 15:33:52 +0200')
+      expect(invalid_booking.errors.full_messages).to include('End at reserved until Mon, 15 Jun 2015 17:33:52 +0200')
+    end
+
+    it 'raises an error if it is between another booking' do
+      valid_booking = Booking.new(bookable: @room, start_at: Time.now - 2.hours, end_at: Time.now - 1.hours)
+      expect(valid_booking).to be_valid
+
+      invalid_booking = Booking.new(bookable: @room, start_at: Time.now + 1.hour + 30.minutes, end_at: Time.now + 2.hours)
+      expect(invalid_booking).not_to be_valid
+      expect(invalid_booking.errors.full_messages).to include('Start at reserved from Mon, 15 Jun 2015 15:33:52 +0200')
+      expect(invalid_booking.errors.full_messages).to include('End at reserved until Mon, 15 Jun 2015 17:33:52 +0200')
+    end
   end
 end
